c530b2d81dc86df14e11db4cea82392efa9d1dca
yeosang0508 <yeosang0508@gmail.com>
2024-07-02 23:26:43 +0900

    article write 구현

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,26 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int id = 1;
+
+        while (true) {
+
+            System.out.print("명령어) ");
+            String order = sc.nextLine();
+
+            if (order.equals("article write")) {
+                System.out.print("제목: ");
+                String title = sc.nextLine();
+                System.out.print("내용: ");
+                String body = sc.nextLine();
+
+                System.out.println(id++ + "번 글이 생성되었습니다.");
+            }
+        }
     }
 }
