f1e6b9f02723a7c3ec0ab63fbf5628da425f30e7
yeosang0508 <yeosang0508@gmail.com>
2024-07-07 23:55:21 +0900

    article list 구현

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,11 +1,17 @@
 package org.koreait;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        List<Article> articles = new ArrayList<>();
 
         int id = 1;
 
@@ -23,16 +29,77 @@ public class Main {
                 System.out.print("내용: ");
                 String body = sc.nextLine();
 
+                Article article = new Article(id, title, body);
+
+                articles.add(article);
 
                 System.out.println(id++ + "번 글이 생성되었습니다");
+
+
+            } else if (cmd.equals("article list")) {
+                System.out.println("번호   /   제목   /   내용");
+
+                for(int i = articles.size() - 1; i >= 0; i--){
+                    Article article = articles.get(i);
+
+                    System.out.println(article.getId() + "   /   " + article.getTitle() + "   /   " + article.getBody());
+                }
+
             }
 
 
-            if(cmd.equals("exit")){
+            if (cmd.equals("exit")) {
                 System.out.println("== 프로그램 종료 ==");
                 break;
             }
 
         }
+    }
+
+}
+
+class Article {
+
+    int id;
+    String title;
+    String body;
+
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
     }
 }
